9ff9f71e5819c2ce22e8f3aaee269a8eb26e5d07
Patrick Dessalle <patrick@dessalle.be>
2020-05-08 23:30:05 +0200

    Setup for pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cmms',
+    version='0.1',
+    description='Python module to interact with Biogen CMMS software (Infor EAM)',
+    url='git@github.com:patrickdessalle/biogen-cmms',
+    author='Patrick Dessalle',
+    author_email='patrick@dessalle.be',
+    license='Proprietary',
+    packages=['cmms'],
+    python_requires='2.7'
+)
