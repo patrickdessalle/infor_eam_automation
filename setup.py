6bb2425e97b7a3291e1dbccf52a453bbb73e7483
Patrick Dessalle <patrick@dessalle.be>
2020-05-15 01:19:24 +0200

    Setup.py change to simpler structure

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='cmms',
-    version='0.1',
+    version='0.2',
     description='Python module to interact with Infor EAM software using Selenium',
     url='git@github.com:patrickdessalle/infor-eam-automation',
     author='Patrick Dessalle',
     author_email='patrick@dessalle.be',
     license='Proprietary',
-    packages=['cmms'],
+    packages=['__init__'],
     python_requires='2.7'
 )
